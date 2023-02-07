dd96ebd90503e2705a47c95dfb7c40937093d5f1
jiaweizhang <jiaweizhang@Jiaweis-Mac-mini.local>
2023-02-07 00:29:41 -0800

    Initial customizations

---
+++ src/content/post/long-title.md
@@ -0,0 +1,8 @@
+---
+title: "Another blog post"
+description: "This post is purely for testing if the css is correct for the title on the page"
+publishDate: "01 Feb 2023"
+tags: ["test"]
+---
+
+## Testing the title tag
---
+++ src/site.config.ts
@@ -0,0 +1,11 @@
+const siteMeta = {
+	author: "Jiawei Zhang",
+	title: "Personal Website",
+	description: "A personal website",
+	lang: "en-GB",
+	ogLocale: "en_GB",
+	themeColorLight: "#fafafa",
+	themeColorDark: "#1d1f21",
+};
+
+export default siteMeta;
