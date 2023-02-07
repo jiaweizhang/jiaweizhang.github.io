c8201d3010e6253d93f7da7c804aaa4ba533d0ae
jiaweizhang <jiaweizhang@Jiaweis-Mac-mini.local>
2023-02-07 01:35:32 -0800

    Stuff

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,20 @@
 npm run build
 
 cd ../jiaweizhang.github.io
+pwd
 git rm -rf .
 git clean -fxd
+pwd 
 
 # copy folder over
 cp -rf ../website/dist/* .
 cp ../website/CNAME .
+pwd 
 
 git add .
 git commit -m "Added commit"
-
+pwd 
 git push -f
-
+pwd 
 cd ../website
+pwd 
